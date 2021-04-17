9a4db0a23c46f2ffb1140fd94b1489d83294cedc
Varad Panke <panke.varad@gmail.com>
2021-04-17 19:24:55 +0530

    Initial commit

---
+++ src/Components/ToDoItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ToDoItem = () => {
+  return <div>ToDo Works!!</div>;
+};
+
+export default ToDoItem;
---
+++ src/Components/ToDos.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ToDoItem from "./ToDoItem";
+
+const ToDos = () => {
+  return (
+    <div>
+      <h1>TODOS</h1>
+      <ToDoItem />
+    </div>
+  );
+};
+
+export default ToDos;
