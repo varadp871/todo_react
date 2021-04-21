0f32d3a8984886d93f8dfaa9a5abf6496c96cd5f
Varad Panke <panke.varad@gmail.com>
2021-04-21 11:25:58 +0530

    formik dependency added

--- src/Components/ToDoItem.js
+++ src/Components/ToDoItem.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const ToDoItem = () => {
-  return <div>ToDo Works!!</div>;
+  const [title, setTitle] = useState();
+  const [desc, setDesc] = useState();
+
+  return (
+    <div className="container">
+    
+      <div className="input-group mb-3">
+        <label htmlFor="title">Enter Title : </label>
+        <input
+          type="text"
+          className="form-control"
+          aria-label="Sizing example input"
+          aria-describedby="inputGroup-sizing-default"
+          name="title"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+      </div>
+      <div className="input-group mb-3">
+        <label htmlFor="desc">Enter Desc : </label>
+        <input
+          type="text"
+          className="form-control"
+          aria-label="Sizing example input"
+          aria-describedby="inputGroup-sizing-default"
+          name="desc"
+          value={desc}
+          onChange={(e) => {
+            setDesc(e.target.value);
+          }}
+        />
+      </div>
+      <button type="button" class="btn btn-outline-warning">
+        Submit
+      </button>
+    </div>
+  );
 };
 
 export default ToDoItem;
